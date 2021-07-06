ab5e10c3ebb160cb6822ebf7a70644275188249e
ghozinash <ghozinash97@gmail.com>
2021-07-06 19:03:23 +0700

    Change "About Me" to "Profile"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                About Me
+                Profile
               </a>
             </li>
             <li className="nav-item">

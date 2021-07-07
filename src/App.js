15835083a1fe1f717a8cc298799322f450c24f6d
ghozinash <ghozinash97@gmail.com>
2021-07-07 17:52:34 +0700

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import { Circle } from "tsparticles/Utils";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
